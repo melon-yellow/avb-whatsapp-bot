98a0429b06b781257a94b75c656e8c29d40a0460
anthony <78743473+anthony-freitas@users.noreply.github.com>
2021-11-08 14:16:37 -0300

    erhezt6ikzitghfjg

--- whatsapp/src/index.ts
+++ whatsapp/src/index.ts
@@ -69,13 +69,11 @@ const port = Number(process.env.WHATSAPP_PORT)
 Array(
   Number(process.env.WHATSAPP_USERS)
 ).forEach((_v, i) => {
-  const user = {
-    [process.env[`WHATSAPP_USER_${i + 1}`]]: (
-      process.env[`WHATSAPP_PASSWORD_${i + 1}`]
-    )
-  }
   // Assign User
-  Object.assign(avbot.network.users, user)
+  Object.assign(avbot.network.users, {
+    [process.env[`WHATSAPP_USER_${i + 1}`]]:
+     process.env[`WHATSAPP_PASSWORD_${i + 1}`]
+  })
 })
 
 /*
